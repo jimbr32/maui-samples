d71b98f12dfed3bbaf872c60c0811b0fa8fe1189
Jim Bretti <james.bretti@honeywell.com>
2024-09-30 09:11:04 -0400

    Add test app MauiApp3

---
+++ MauiApp3/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnTestPageClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Starting 10 second delay");
+            await Task.Delay(10000);
+            Console.WriteLine("Before PushAsync for TestPage");
+            await Navigation.PushAsync(new TestPage());
+            Console.WriteLine("After PushAsync for TestPage");
+        }
+    }
+
+}
---
+++ MauiApp3/MauiApp3/TestPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace MauiApp3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
